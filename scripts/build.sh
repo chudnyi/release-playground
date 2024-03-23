691790f8953ebe9cf51763f486004a045a05a5f1
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2024-03-23 19:58:48 +0300

    feat: build, use matrix for build target 01

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,15 +4,8 @@ SELF_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$SELF_DIR/.."
 source "$SELF_DIR/vars.env"
 
-declare -a targets=(
-  x86_64-unknown-linux-gnu
-  aarch64-unknown-linux-gnu
-  x86_64-pc-windows-msvc
-  x86_64-apple-darwin
-  aarch64-apple-darwin
-)
-
-for target in "${targets[@]}"; do
+build() {
+  local target="$1"
   output_bin="play-$target"
   output_zip="$output_bin.zip"
   rm -fr "$ROOT_DIR/dist" && mkdir -p "$ROOT_DIR/dist"
@@ -21,4 +14,14 @@ for target in "${targets[@]}"; do
   pkgx "$DENO" compile -A --output "./$output_bin" --target "$target" "$ROOT_DIR/src/cli.ts"
   pkgx "$SEVENZ" a "$output_zip" "$output_bin"
   rm -f "$output_bin"
-done
+}
+
+build "$@"
+
+#declare -a targets=(
+#  x86_64-unknown-linux-gnu
+#  aarch64-unknown-linux-gnu
+#  x86_64-pc-windows-msvc
+#  x86_64-apple-darwin
+#  aarch64-apple-darwin
+#)
