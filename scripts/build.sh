9624f933b256538aa85dec37f69c0da3cde63a65
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2024-03-23 18:48:33 +0300

    fix: debug build, try upload artifacts 03

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,17 +5,17 @@ ROOT_DIR="$SELF_DIR/.."
 source "$SELF_DIR/vars.env"
 
 declare -a targets=(
-#  x86_64-unknown-linux-gnu
-#  aarch64-unknown-linux-gnu
-#  x86_64-pc-windows-msvc
-#  x86_64-apple-darwin
+  x86_64-unknown-linux-gnu
+  aarch64-unknown-linux-gnu
+  x86_64-pc-windows-msvc
+  x86_64-apple-darwin
   aarch64-apple-darwin
 )
 
 for target in "${targets[@]}"; do
   output_bin="play-$target"
   output_zip="$output_bin.zip"
-  mkdir -p "$ROOT_DIR/dist"
+  rm -fr "$ROOT_DIR/dist" && mkdir -p "$ROOT_DIR/dist"
   cd "$ROOT_DIR/dist"
   rm -f "$output_bin" "$output_zip"
   pkgx "$DENO" compile -A --output "./$output_bin" --target "$target" "$ROOT_DIR/src/cli.ts"
