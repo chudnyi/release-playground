8bfdc7415e4f48b05075baf3764f552bbfeb3add
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2024-03-24 18:12:17 +0300

    feat: take version from package.json

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env -S deno run
 import { Command } from "./deps/cliffy.ts";
+import packageJson from "../package.json" with { type: "json" };
 
 await new Command()
   .name("play")
+  .version(packageJson.version)
   .action(() => {
     console.log("Play!!!");
   })
